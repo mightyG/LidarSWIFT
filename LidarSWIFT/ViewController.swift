34edb32c28109f72979869c3571231b98c471727
Georgio Sayegh <georgio-sayegh@hotmail.com>
2022-06-03 10:49:39 +0300

    Initial Commit

---
+++ LidarSWIFT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LidarSWIFT
+//
+//  Created by Georgio Sayegh on 03/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
